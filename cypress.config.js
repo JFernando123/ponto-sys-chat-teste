9d5eb51f44600be59d549e8c394fed77e0a76c10
José Fernando de Miranda <jfernando.etec@gmail.com>
2025-01-05 19:11:36 -0300

    1.0

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,4 @@ module.exports = defineConfig({
     env:{ hideXr: true},
     experimentalModifyObstructiveThirdPartyCode: true,
   },
-  fixturesFolder: false,
 });
--- cypress/e2e/chat-ps.cy.js
+++ cypress/e2e/chat-ps.cy.js
@@ -21,21 +21,6 @@ describe('cy.origin()',()=>{
       cy.get('#chat-especialidade').type('Sem acesso ao sistema / Vendas / Operação de caixa')
       cy.get('#chat-descricao').type('Teste para Iniciar Atendimento')
 
-      cy.get('button[type="submit"]').click()
-
-      cy.url().should('eq', 'https://contact.adtechtelecom.com.br/chatclient/chat.php');
-
-      cy.wait(5000)
-
-      cy.get('[data-i18n="action.finalizar"]').should('be.visible').click()   
-
-
-      //cy.origin('https://contact.adtechtelecom.com.br/chatclient/chat.php', ()=> {        
-      //  cy.get('button #end-chat-up').click()
-      //})
-
-      //cy.get('button[type="submit"]').click().wait(2000).contains('button','Cancelar').click()
-      //cy.wait(4000)
-      //cy.contains('button[type="submit"]','Finalizar').click()
+      cy.get('button[type="submit"]').click()  
   })
 })
